057f9d7d1830d83692c259a02a785ab1268fac2d
Artem Romanenia <artem.romanenia@gmail.com>
2016-09-01 23:44:07 +0300

    First commit

---
+++ SharpNjector/CustomTools/Base/SharpNjectorCustomTool.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SharpNjector.CustomTools.Base
+{
+    public abstract class SharpNjectorCustomTool : IVsSingleFileGenerator
+    {
+        public abstract string GetExtension();
+
+        public int DefaultExtension(out string defaultExtension)
+        {
+            defaultExtension = GetExtension();
+            return VSConstants.S_OK;
+        }
+
+        public int Generate(string inputFilePath, string inputFileContents, string defaultNamespace, IntPtr[] outputFileContents, out uint outputSize, IVsGeneratorProgress generateProgress)
+        {
+            byte[] outputBytes = Encoding.UTF8.GetBytes(inputFileContents.ToUpperInvariant());
+            if (outputBytes != null)
+            {
+                outputSize = (uint)outputBytes.Length;
+                outputFileContents[0] = Marshal.AllocCoTaskMem(outputBytes.Length);
+                Marshal.Copy(outputBytes, 0, outputFileContents[0], outputBytes.Length);
+            }
+            {
+                outputFileContents[0] = IntPtr.Zero;
+                outputSize = 0;
+            }
+            return 0;
+        }
+    }
+}
---
+++ SharpNjector/CustomTools/SharpNjectorJsCustomTool.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.Shell;
+using SharpNjector.CustomTools.Base;
+using System;
+using System.Runtime.InteropServices;
+using VSLangProj80;
+
+namespace SharpNjector.CustomTools
+{
+    [ComVisible(true)]
+    [Guid("c6f910ea-4b0d-41d4-94c2-eefafa80574e")]
+    [CodeGeneratorRegistration(typeof(SharpNjectorJsCustomTool), "Generates js file", vsContextGuids.vsContextGuidVCSProject, GeneratesDesignTimeSource = true, GeneratorRegKeyName = "SharpNjectorJsCustomTool")]
+    [ProvideObject(typeof(SharpNjectorJsCustomTool))]
+    public class SharpNjectorJsCustomTool : SharpNjectorCustomTool
+    {
+        public override string GetExtension()
+        {
+            return ".js";
+        }
+    }
+}
